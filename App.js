faca8b3cc5752d219b5718c4bcd9733a3c531418
Ran Tayar <tgmrp.dev@gmail.com>
2021-04-12 11:32:33 +0300

    init

---
+++ App.js
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import Bird from './components/Bird';
+import Obstacles from './components/Obstacles';
+
+export default function App() {
+  const screenWidth = Dimensions.get('screen').width;
+  const screenHeight = Dimensions.get('screen').height;
+  const birdLeft = screenWidth / 2;
+  const [birdBottom, setBirdBottom] = useState(screenHeight / 2);
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth);
+  const [obstaclesLeftTwo, setObstaclesLeftTwo] = useState(
+    screenWidth + screenWidth / 2 + 30
+  );
+  const [obstaclesNegHeight, setObstaclesNegHeight] = useState(0);
+  const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const gravity = 3;
+  let obstacleWidth = 60;
+  let obstacleHeight = 300;
+  let gap = 200;
+  let gameTimerId;
+  let obstaclesTimerId;
+  let obstaclesTimerIdTwo;
+
+  //start bird falling
+  useEffect(() => {
+    if (birdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        setBirdBottom((birdBottom) => birdBottom - gravity);
+      }, 30);
+
+      return () => {
+        clearInterval(gameTimerId);
+      };
+    }
+    //if i dont have birdBottom as a dependecy, it wont stop
+  }, [birdBottom]);
+  console.log(birdBottom);
+
+  const jump = () => {
+    if (!isGameOver && birdBottom < screenHeight) {
+      setBirdBottom((birdBottom) => birdBottom + 50);
+      console.log('jumped');
+    }
+  };
+
+  //start first obstacle
+  useEffect(() => {
+    if (obstaclesLeft > -60) {
+      obstaclesTimerId = setInterval(() => {
+        setObstaclesLeft((obstaclesLeft) => obstaclesLeft - 5);
+      }, 30);
+      return () => {
+        clearInterval(obstaclesTimerId);
+      };
+    } else {
+      setScore((score) => score + 1);
+      setObstaclesLeft(screenWidth);
+      setObstaclesNegHeight(-Math.random() * 100);
+    }
+  }, [obstaclesLeft]);
+
+  //start second obstacle
+  useEffect(() => {
+    if (obstaclesLeftTwo > -60) {
+      obstaclesTimerIdTwo = setInterval(() => {
+        setObstaclesLeftTwo((obstaclesLeftTwo) => obstaclesLeftTwo - 5);
+      }, 30);
+      return () => {
+        clearInterval(obstaclesTimerIdTwo);
+      };
+    } else {
+      setScore((score) => score + 1);
+      setObstaclesLeftTwo(screenWidth);
+      setObstaclesNegHeightTwo(-Math.random() * 100);
+    }
+  }, [obstaclesLeftTwo]);
+
+  //check for collisions
+  useEffect(() => {
+    console.log(obstaclesLeft);
+    console.log(screenWidth / 2);
+    console.log(obstaclesLeft > screenWidth / 2);
+    if (
+      ((birdBottom < obstaclesNegHeight + obstacleHeight + 30 ||
+        birdBottom > obstaclesNegHeight + obstacleHeight + gap - 30) &&
+        obstaclesLeft > screenWidth / 2 - 30 &&
+        obstaclesLeft < screenWidth / 2 + 30) ||
+      ((birdBottom < obstaclesNegHeightTwo + obstacleHeight + 30 ||
+        birdBottom > obstaclesNegHeightTwo + obstacleHeight + gap - 30) &&
+        obstaclesLeftTwo > screenWidth / 2 - 30 &&
+        obstaclesLeftTwo < screenWidth / 2 + 30)
+    ) {
+      console.log('game over');
+      gameOver();
+    }
+  });
+
+  const gameOver = () => {
+    clearInterval(gameTimerId);
+    clearInterval(obstaclesTimerId);
+    clearInterval(obstaclesTimerIdTwo);
+    setIsGameOver(true);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={jump}>
+      <View style={styles.container}>
+        <Bird birdBottom={birdBottom} birdLeft={birdLeft} />
+        <Text style={styles.score}>{score}</Text>
+        {isGameOver && (
+          <>
+            <Text
+              style={{
+                position: 'absolute',
+                width: '100%',
+                textAlign: 'center',
+                bottom: screenHeight / 2,
+                fontSize: 60,
+              }}
+            >
+              Game Over!
+            </Text>
+            <Text
+              style={{
+                position: 'absolute',
+                width: '100%',
+                textAlign: 'center',
+                bottom: screenHeight / 2 - 30,
+                fontSize: 30,
+              }}
+            >
+              Score: {score}
+            </Text>
+          </>
+        )}
+        <Obstacles
+          color={'green'}
+          obstacleWidth={obstacleWidth}
+          obstacleHeight={obstacleHeight}
+          randomBottom={obstaclesNegHeight}
+          gap={gap}
+          obstaclesLeft={obstaclesLeft}
+        />
+        <Obstacles
+          color={'yellow'}
+          obstacleWidth={obstacleWidth}
+          obstacleHeight={obstacleHeight}
+          randomBottom={obstaclesNegHeightTwo}
+          gap={gap}
+          obstaclesLeft={obstaclesLeftTwo}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  score: {
+    position: 'absolute',
+    fontSize: 30,
+    marginTop: 25,
+    marginLeft: 25,
+    zIndex: 999,
+  },
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# flappy-birds-react-native
+
+A game of flappy birds in react native
+set up with [expo](https://docs.expo.io/).
